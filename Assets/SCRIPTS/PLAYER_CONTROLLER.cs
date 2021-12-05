267a15027a32adce2fea01dcebf2dc7749199a23
Volk_115 <mcarrot115@gmail.com>
2021-12-05 17:54:13 +0100

    A

--- Assets/SCRIPTS/PLAYER_CONTROLLER.cs
+++ Assets/SCRIPTS/PLAYER_CONTROLLER.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PLAYER_CONTROLLER : MonoBehaviour
 {
+    //TEXTO QUE PUEDE VARIAR SEGUN LA TECLA SELECCIONADA
+    public TextMeshProUGUI arrowText;
 
-    
     void Start()
     {
-        
+        //TEXTO DE INICIO
+        arrowText.text = $"Pulsa cualquiera de las cuatro flechas";
     }
 
-    
     void Update()
     {
-        
+        //TEXTO ASIGNADO SEGUN LA TECLA PULSADA
+        ShowMessage(KeyCode.LeftArrow, $"Izquierda");
+        ShowMessage(KeyCode.RightArrow, $"Derecha");
+        ShowMessage(KeyCode.UpArrow, $"Arriba");
+        ShowMessage(KeyCode.DownArrow, $"Abajo");
+        ShowMessage(KeyCode.Return, $"¡Pium!");
+    }
+
+    //FUNCION CON KEYCODE Y STRING
+    public void ShowMessage(KeyCode key, string message)
+    {
+        if (Input.GetKey(key))
+        { arrowText.text = message; }
     }
 }
